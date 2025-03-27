1e75ff1d68d72619ce63e0c1b5fe46f8b97ea9bf
Saul Stepchenkov <Savelijfamilia130@gmail.com>
2025-03-27 12:44:30 +0300

    В android создал вход для учителя

---
+++ android/app/src/main/java/com/example/message/activity/SignInTeacherActivity.java
@@ -0,0 +1,40 @@
+package com.example.message.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.message.R;
+
+public class SignInTeacherActivity extends AppCompatActivity {
+    Intent SignInActivity;
+    TextView signin_bottom_click;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in_teacher);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SignInActivity = new Intent(this, SignInActivity.class);
+
+
+        signin_bottom_click = findViewById(R.id.signin_bottom_click);
+
+
+        signin_bottom_click.setOnClickListener(view -> {
+            startActivity(SignInActivity);
+        });
+
+    }
+}
